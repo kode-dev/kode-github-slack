418b2106ddfbb79c0a2631bf875cb2211c4d330b
Steven Jing <steven.jing@gmail.com>
2018-09-09 11:12:42 -0700

    Setup github octonode starting code

---
+++ github.js
@@ -0,0 +1,37 @@
+const app = require('./app.js');
+const github = require('octonode');
+
+// ENV
+const dotenv = require('dotenv');
+dotenv.load();
+
+const client = github.client(process.env.GITHUB_ACCESS_TOKEN);
+
+client.get('/user', {}, function (err, status, body, headers) {
+  console.log(body); //json object
+});
+
+// var ghme           = client.me();
+// var ghuser         = client.user('pksunkara');
+// var ghrepo         = client.repo('pksunkara/hub');
+// var ghorg          = client.org('flatiron');
+// var ghissue        = client.issue('pksunkara/hub', 37);
+// var ghmilestone    = client.milestone('pksunkara/hub', 37);
+// var ghlabel        = client.label('pksunkara/hub', 'todo');
+// var ghpr           = client.pr('pksunkara/hub', 37);
+// var ghrelease      = client.release('pksunkara/hub', 37);
+// var ghgist         = client.gist();
+// var ghteam         = client.team(37);
+// var ghproject      = client.project('pksunkara/hub', 37);
+// var ghnotification = client.notification(37);
+
+// var ghsearch = client.search();
+
+app.get('/clone_repo', function (req, res) {
+
+    // client.get('/users/stevenjing', {}, function (err, status, body, headers) {
+    //     console.log(body); //json object
+    // });
+
+    res.send('Hello, world!');
+});
